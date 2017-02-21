3a2df086980ee7835d5fc0479ce1aa1bd3690eb6
Yeremy <yeremyturcios@gmail.com>
2017-02-21 09:38:09 -0700

    Changed POST to GET. Deleted unnecessary @SpringBootApplication reference.

--- src/main/java/org/yeremy/giftexchange/web/GiftExchangeController.java
+++ src/main/java/org/yeremy/giftexchange/web/GiftExchangeController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,6 @@ import org.yeremy.giftexchange.dto.GiftSet;
 import io.swagger.annotations.Api;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
 @EnableSwagger2
 @RestController
 @ComponentScan("org.yeremy.giftexchange")
@@ -29,7 +27,7 @@ public class GiftExchangeController
     @Inject
     private GiftExchangeService giftExchangeService;
 
-    @RequestMapping(path = "/{familyGroup}", method = RequestMethod.POST, produces = "application/json", consumes = "text/plain")
+    @RequestMapping(path = "/{familyGroup}", method = RequestMethod.GET, produces = "application/json")
     public List<GiftSet> getGiftExchangeList(@PathVariable("familyGroup") String familyGroup,
             @RequestParam(value = "record") Boolean record)
     {
